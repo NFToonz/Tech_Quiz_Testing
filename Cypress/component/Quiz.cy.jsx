f6d7ec3cfb09f2556df71bb711763fc08f9631ea
Toonz <johnnyt3.2014@gmail.com>
2025-05-12 23:13:39 -0400

    edited cypress Quiz and tsconfig file, test somewhat runing

--- Cypress/component/Quiz.cy.jsx
+++ Cypress/component/Quiz.cy.jsx
@@ -1,36 +1,17 @@
 // Component test for the Quiz component
-import Quiz from '../../client/src/components/Quiz.js';
+import Quiz from '../../client/src/components/Quiz';
 
 describe ('Quiz component', () => {
    // Mock the fetch function
    beforeEach(() => { // Reset the intercept before each test
-        cy.intercept('GET', '/src/quiz', {
-            statusCode: 200, // Simulate/Mock a successful response
-            body: [{
-                question : 'What is 2 + 2?',
-                answers : [
-                    { answer: '3', isCorrect: false },
-                    { answer: '4', isCorrect: true },
-                    { answer: '5', isCorrect: false }
-                ],
-            },
-            {
-                question : 'What is the capital of France?',
-                answers : [
-                    { answer: 'Berlin', isCorrect: false },
-                    { answer: 'Madrid', isCorrect: false },
-                    { answer: 'Paris', isCorrect: true }
-                ],
-            }]
-            });
-        
-        });
-    });
+        cy.intercept('GET', '/src/quiz');
+           
+            
 
     // Test cases for the Quiz component - adds more
     it('renders the quiz component', () => {
         cy.mount(<Quiz />); // Mount the Quiz component in the test environment
-        cy.contains('Start Quiz').should('be.visible'); // Check if the "Start Quiz" button is visible
+        cy.get('button').contains('Start Quiz').should('be.visible'); // Check if the "Start Quiz" button is visible
     });
 
     it('fetches quiz data and displays questions', () => {
@@ -56,15 +37,5 @@ describe ('Quiz component', () => {
         cy.contains('Next').click(); // Simulate a click on the "Next" button
         cy.contains('What is the capital of France?').should('be.visible'); // Check if the second question is displayed
     }
-    );
-    // it('shows the result when the quiz is completed', () => {
-    //     mount(<Quiz />);
-    //     cy.contains('Start Quiz').click(); // Simulate a click on the "Start Quiz" button
-    //     cy.wait('@getQuiz'); // Wait for the quiz data to be fetched
-    //     cy.contains('4').click(); // Simulate a click on the right answer
-    //     cy.contains('Next').click(); // Simulate a click on the "Next" button
-    //     cy.contains('Paris').click(); // Simulate a click on the right answer
-    //     cy.contains('Finish').click(); // Simulate a click on the "Finish" button
-    //     cy.contains('You scored 2 out of 2!').should('be.visible'); // Check if the result is displayed
-    // });
-    
+    );}
+)})
